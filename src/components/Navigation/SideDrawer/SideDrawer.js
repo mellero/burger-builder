55c3bfb06e2df9c1d77ad47c1f4ad2e9ee229e34
mellero <m_ellero@hotmail.com>
2019-05-14 19:34:34 -0400

    Adds responsive design to app. Adds functionality to SideDrawer.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = ( props ) => {
+    const attachedClasses = [classes.SideDrawer, classes.Closed];
+    if (props.show) attachedClasses[1] = classes.Open;
 
     return (
-        <div className={classes.SideDrawer}>
-            <Logo />
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Fragment>
+            <Backdrop show={props.show} exit={props.exit}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}><Logo /></div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Fragment>
     );
 }
 
